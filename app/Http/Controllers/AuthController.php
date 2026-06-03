25cca1aaaec2d1e56941e9dec25411d65fc62845
Jason Momanyi <orandijasons@gmail.com>
2026-06-03 21:45:25 +0300

    add register route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,27 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller {
+    
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'role' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'user' => $user]);
+    }
+
     public function login(Request $request) {
         $credentials = $request->validate([
             'email' => 'required|email',
