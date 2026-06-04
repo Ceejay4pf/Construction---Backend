416ae9b242d929c0cc6d8ade45b4452f28ac56fd
Jason Momanyi <orandijasons@gmail.com>
2026-06-04 06:04:29 +0300

    add register route to api

--- routes/api.php
+++ routes/api.php
@@ -13,53 +13,37 @@ use App\Http\Controllers\MpesaController;
 
 // Public routes
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/mpesa/callback', [MpesaController::class, 'callback']);
 
-// Protected routes
+// TEMP - make all routes public for testing
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::post('/projects', [ProjectController::class, 'store']);
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
+Route::put('/projects/{id}', [ProjectController::class, 'update']);
+Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
+Route::get('/projects/{projectId}/floors', [FloorController::class, 'index']);
+Route::put('/floors/{id}', [FloorController::class, 'update']);
+Route::get('/projects/{projectId}/boq', [BoqController::class, 'index']);
+Route::post('/boq', [BoqController::class, 'store']);
+Route::put('/boq/{id}', [BoqController::class, 'update']);
+Route::delete('/boq/{id}', [BoqController::class, 'destroy']);
+Route::get('/projects/{projectId}/invoices', [LabourInvoiceController::class, 'index']);
+Route::post('/invoices', [LabourInvoiceController::class, 'store']);
+Route::put('/invoices/{id}', [LabourInvoiceController::class, 'update']);
+Route::delete('/invoices/{id}', [LabourInvoiceController::class, 'destroy']);
+Route::get('/projects/{projectId}/payments', [PaymentController::class, 'index']);
+Route::post('/payments', [PaymentController::class, 'store']);
+Route::get('/projects/{projectId}/materials', [MaterialController::class, 'index']);
+Route::post('/materials', [MaterialController::class, 'store']);
+Route::put('/materials/{id}', [MaterialController::class, 'update']);
+Route::delete('/materials/{id}', [MaterialController::class, 'destroy']);
+Route::get('/projects/{projectId}/progress', [SiteProgressController::class, 'index']);
+Route::post('/progress', [SiteProgressController::class, 'store']);
+Route::post('/mpesa/stk-push', [MpesaController::class, 'stkPush']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
-
-    // Dashboard
-    Route::get('/dashboard', [DashboardController::class, 'index']);
-
-    // Projects
-    Route::get('/projects', [ProjectController::class, 'index']);
-    Route::post('/projects', [ProjectController::class, 'store']);
-    Route::get('/projects/{id}', [ProjectController::class, 'show']);
-    Route::put('/projects/{id}', [ProjectController::class, 'update']);
-    Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
-
-    // Floors
-    Route::get('/projects/{projectId}/floors', [FloorController::class, 'index']);
-    Route::put('/floors/{id}', [FloorController::class, 'update']);
-
-    // BOQ
-    Route::get('/projects/{projectId}/boq', [BoqController::class, 'index']);
-    Route::post('/boq', [BoqController::class, 'store']);
-    Route::put('/boq/{id}', [BoqController::class, 'update']);
-    Route::delete('/boq/{id}', [BoqController::class, 'destroy']);
-
-    // Labour Invoices
-    Route::get('/projects/{projectId}/invoices', [LabourInvoiceController::class, 'index']);
-    Route::post('/invoices', [LabourInvoiceController::class, 'store']);
-    Route::put('/invoices/{id}', [LabourInvoiceController::class, 'update']);
-    Route::delete('/invoices/{id}', [LabourInvoiceController::class, 'destroy']);
-
-    // Payments
-    Route::get('/projects/{projectId}/payments', [PaymentController::class, 'index']);
-    Route::post('/payments', [PaymentController::class, 'store']);
-
-    // Materials
-    Route::get('/projects/{projectId}/materials', [MaterialController::class, 'index']);
-    Route::post('/materials', [MaterialController::class, 'store']);
-    Route::put('/materials/{id}', [MaterialController::class, 'update']);
-    Route::delete('/materials/{id}', [MaterialController::class, 'destroy']);
-
-    // Site Progress
-    Route::get('/projects/{projectId}/progress', [SiteProgressController::class, 'index']);
-    Route::post('/progress', [SiteProgressController::class, 'store']);
-
-    // Mpesa STK Push
-    Route::post('/mpesa/stk-push', [MpesaController::class, 'stkPush']);
 });
